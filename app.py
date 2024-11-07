67e19f26b22e92fcc25bf3b664b7cb3b954bbd93
Perez, Blas Sebastian <bsperez@fi.uba.ar>
2024-11-07 01:25:19 -0300

    Agrego endpoints de reserva, pago, futbol y tu-reserva

--- app.py
+++ app.py
@@ -6,5 +6,27 @@ app = Flask(__name__)
 def index():
     return render_template('pago.html')
 
+@app.route('/futbol')
+def Futbol():
+    return render_template('futbol.html')
+
+@app.route('/reserva')
+def Reserva():
+    return render_template('reserva.html')
+
+@app.route('/pago')
+def Pago():
+    return render_template('pago.html')
+
+@app.route('/tu-reserva')
+def Tu_reserva():
+    nombre="Juan Perez"
+    dni="42100124"
+    codigo_reserva="ab1224"
+    sector="7B"
+    cantidad_tickets=2
+    precio_total = "$40.000"
+    return render_template('tu-reserva.html', nombre=nombre,dni=dni, codigo_reserva=codigo_reserva, sector=sector, cantidad_tickets=cantidad_tickets, precio_total=precio_total)
+
 if __name__ == '__main__':
     app.run(debug=True)
