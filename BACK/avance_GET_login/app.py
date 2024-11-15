b5684922d409f37174c2f1604517060581b926bd
Perez, Blas Sebastian <bsperez@fi.uba.ar>
2024-11-15 01:31:52 -0300

    Creo endpoint Metodo GET consultar-reserva, (ID=#35)

--- BACK/avance_GET_login/app.py
+++ BACK/avance_GET_login/app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from werkzeug.security import check_password_hash
 from config import Config
 from models import db, Usuario
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+
+QUERY_RESERVA_BY_ID = "SELECT id_usuarios, id_evento, cant_tickets FROM reservas WHERE id_reserva = :id_reserva"
+
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 
 db.init_app(app)
+
+engine = create_engine("mysql+mysqlconnector://root:tuclave@localhost:3306/universe")
+
+def run_query(query, parameters=None):
+    with engine.connect() as conn:
+        result = conn.execute(text(query), parameters)
+        conn.commit()
+
+    return result
 
 """ with app.app_context():
     db.create_all() """
@@ -38,6 +54,33 @@ def login():
 @app.route('/dashboard')
 def dashboard():
     return "¡Bienvenido al Dashboard!"
+
+
+#--------------------------------------------------------TABLA RESERVAS-----------------------------------#
+
+
+#----METODO GET, CONSULTAR RESERVA POR ID DE RESERVA -----#
+
+def reserva_by_id(id_reserva):
+    return run_query(QUERY_RESERVA_BY_ID, {'id_reserva': id_reserva}).fetchall()
+
+@app.route('/consultar-reserva/<int:id_reserva>', methods=['GET'])
+def get_by_padron(id_reserva):
+    try:
+        result = reserva_by_id(id_reserva)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    if len(result) == 0:
+        return jsonify({'error': 'No se encontró la reserva'}), 404 # Not found
+
+    result = result[0]
+    return jsonify({'id_usuarios': result[0], 'id_evento': result[1], 'cant_tickets': result[2]}), 200
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
