ecc88c308fa5bbd0fe1ef90153bef4f1e87f10c9
alejohdiaz <alejofvc@gmail.com>
2024-11-19 19:43:28 -0300

    cambio nombre de funcion de Reserva a reserva

--- FRONT/app.py
+++ FRONT/app.py
@@ -3,36 +3,42 @@ import requests
 
 app = Flask(__name__)
 
+# Variable para validar si es admin o no,
+# app.config es un diccionario especial de flask para almacenar configuraciones de la aplicación,
+# estas estan disponibles en toda la aplicacion y son accesibles desde cualquier parte de la misma
+app.config['ES_ADMIN'] = True
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/musica')
 def musica():
-    return render_template('musica.html')
+    return render_template('musica.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/cultura_jp')
 def cultura_jp():
-    return render_template('cultura_jp.html')
+    return render_template('cultura_jp.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/fiestas')
 def fiestas():
-    return render_template('fiestas.html')
+    return render_template('fiestas.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/eSports')
 def esports():
-    return render_template('eSports.html')
+    return render_template('eSports.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/error')
 def error():
-    return render_template('error.html')
+    return render_template('error.html', es_admin=app.config['ES_ADMIN'])
+
 
 @app.route('/reserva', methods=["GET", "POST"])
-def Reserva():
+def reserva():
     if request.method == "POST":
         id_reserva = request.form.get('id_reserva')
 
@@ -54,7 +60,7 @@ def Reserva():
                 cant_tickets=datos_reserva['cant_tickets']
 
                 return render_template(
-                'tu-reserva.html',  id_reserva=id_reserva,
+                'tu-reserva.html', es_admin=app.config['ES_ADMIN'],  id_reserva=id_reserva,
                                     cant_tickets=cant_tickets, 
                                     nombre_usuario=nombre_usuario, 
                                     nombre_evento=nombre_evento, 
@@ -67,11 +73,11 @@ def Reserva():
         except Exception as e:
             current_app.logger.error(f'Unexpected error: {e}')
             return str(e), 500
-    return render_template('reserva.html')
+    return render_template('reserva.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/pago')
 def Pago():
-    return render_template('pago.html')
+    return render_template('pago.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/futbol')
 def Futbol():
@@ -82,15 +88,15 @@ def Futbol():
              {'nombre':"Visita al museo de Boca Juniors", 'descripcion':'Disfruta una visita al museo de uno de los mejores equipos de Argentina y America'},
              {'nombre':"Visita al museo de Boca Juniors", 'descripcion':'Disfruta una visita al museo de uno de los mejores equipos de Argentina y America'}]
 
-    return render_template('futbol.html', nombre_categoria=nombre_categoria,descripcion_categoria=descripcion_categoria, eventos=eventos )
+    return render_template('futbol.html', es_admin=app.config['ES_ADMIN'], nombre_categoria=nombre_categoria,descripcion_categoria=descripcion_categoria, eventos=eventos )
 
 @app.route('/stand-up')
 def Stand_up():
-    return render_template('stand_up.html')
+    return render_template('stand_up.html', es_admin=app.config['ES_ADMIN'])
 
 @app.route('/teatro')
 def Teatro():
-    return render_template('Teatro.html')
+    return render_template('Teatro.html', es_admin=app.config['ES_ADMIN'])
 
 
 if __name__ == '__main__':
