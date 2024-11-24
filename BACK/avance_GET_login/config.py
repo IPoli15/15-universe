d7b0aac6d38af77969d19e7a61cae336821e6fc1
Yohsz <Yohara.Szinvelski@gmail.com>
2024-11-23 22:07:13 -0300

    Agregado directorios BACK y FRONT

--- BACK/avance_GET_login/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecretkey'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/universe'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
