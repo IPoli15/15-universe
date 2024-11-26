d5b15b71aec3178c687e2c5e2068da56ebcdfa68
IPoli15 <ipoli@fi.uba.ar>
2024-11-26 13:42:57 -0300

    #81

--- APP/main.py
+++ APP/main.py
@@ -1,9 +1,13 @@
+from kivy.uix.filechooser import Screen
+from kivy.uix.screenmanager import SlideTransition
 import kivy
 from kivy.app import App
+from kivy.lang import Builder 
 from kivy.core.text import LabelBase
 # from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from utils.colors import new_rgb_color
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
 
 kivy.require('2.3.0')
@@ -19,12 +23,19 @@ LabelBase.register(
 class MyRoot(BoxLayout):
     def __init__(self):
         super(MyRoot, self).__init__()
+    
+    def switch(self):
+        app.screen_manager.transition = SlideTransition(direction='down')
+        app.screen_manager.current = 'Res'
 
     def change_color(self):
         self.main_title.color = new_rgb_color()
 
 class Reserva(BoxLayout):
-      def change_color(self):
+    def __init__(self):
+        super(Reserva, self).__init__()
+    
+    def change_color(self):
         self.main_title.color = new_rgb_color()
       
 
@@ -37,7 +48,21 @@ class Universe(App):
 
     def build(self):
         # return Label(text="Hello World!")
-        return MyRoot()
+        # return MyRoot()
+        self.screen_manager = ScreenManager()
+
+        self.myroot = MyRoot()
+        screen = Screen(name='Root')
+        screen.add_widget(self.myroot)
+        self.screen_manager.add_widget(screen)
+
+        self.reserva = Reserva()
+        screen = Screen(name='Res')
+        screen.add_widget(self.reserva)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
+
 
     
 if __name__ == "__main__":
