faef49f6af3e2f886df04d8ade1a8b16b31ba702
Ruy Mori <ruymori630@gmail.com>
2024-11-27 16:05:40 -0300

    cambio configuraciones para la conexión a la base de datos

--- BACK/config.py
+++ BACK/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/universe'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:universe1234@Ruy.mysql.pythonanywhere-services.com/universe'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
